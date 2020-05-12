9ca000911a1e9c11b6e516a1d36e3d062f1cd2ad
Akshay-cita <edakkatakshay@gmail.com>
2020-05-12 21:13:21 +0530

    UserProfileInfo model created

--- Blog/models.py
+++ Blog/models.py
@@ -1,35 +1,39 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
- class Post(models.Model):
-     author=models.ForeignKey('auth.User', on_delete=models.CASCADE)
-     title=models.CharField(max_length=200)
-     text=models.TextField()
-     create_date=models.DateTimeField(default=timezone.now()) #publish date and time
-     published_date=models.DateTimeField(blank=True,null=True)
+# Create your models here.
+class Post(models.Model):
+    author=models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    text=models.TextField()
+    create_date=models.DateTimeField(default=timezone.now()) #publish date and time
+    published_date=models.DateTimeField(blank=True,null=True)
 
 
-     def approve_comment(self):
-         return self.comments.filter(approved_comment=True)
+    def approve_comment(self):
+        return self.comments.filter(approved_comment=True)
 
-     def get_absolute_url(self):
-         return reverse("post_detail" kwargs={'pk':self.pk})
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={'pk':self.pk})
 
 
-     def publish(self):
-         self.published_date=timezone.now()
-         self.save()
+    def publish(self):
+        self.published_date=timezone.now()
+        self.save()
 
-     def __str__(self):
-         self.title
-class Comment(model.Model):
-    post=models.ForeignKey('Blog.Post',related_name='comments')
+    def __str__(self):
+        self.title
+class Comment(models.Model):
+    post=models.ForeignKey('Blog.Post',related_name='comments',on_delete=models.CASCADE)
     author=models.CharField(max_length=200)
     text=models.TextField()
     create_date=models.DateTimeField(default=timezone.now())
-    approved_comment=models.BooleanFeild(default=False)
+    approved_comment=models.BooleanField(default=False)
+
+
 
     def approve(self):
         self.approved_comment=True
@@ -39,3 +43,12 @@ class Comment(model.Model):
 
     def __str__(self):
         return self.text
+
+
+class UserProfileInfo(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    portfolio_site=models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        return self.user.username
