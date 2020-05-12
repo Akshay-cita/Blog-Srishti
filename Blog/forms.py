bee76f998aace49e6c46c223ce952dbf8edd78a5
Akshay-cita <edakkatakshay@gmail.com>
2020-05-12 21:12:02 +0530

    Created login and signup page

--- Blog/forms.py
+++ Blog/forms.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from django import forms
-from Blog.models import Comment,Post
+from Blog.models import Comment,Post,UserProfileInfo
 
 class PostForm(forms.ModelForm):
     class Meta():
@@ -17,3 +18,12 @@ class CommentForm(forms.ModelForm):
             'author':forms.TextInput(attrs={'class':'textinputclass'}),
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
         }
+class UserForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput())
+    class Meta():
+        model= User
+        fields=('username','email','password')
+class UserProfileInfoform(forms.ModelForm):
+    class Meta():
+        model=UserProfileInfo
+        fields=('portfolio_site','profile_pic')
