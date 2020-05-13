83365bbbd8df22061f81332d6a8d307db9fe8592
Akshay-cita <edakkatakshay@gmail.com>
2020-05-14 00:02:45 +0530

    imported Userprofileinfo model

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Blog.models import Post,Comment
+from Blog.models import Post,Comment,UserProfileInfo
 
 # Register your models here.
 admin.site.register(Post)
